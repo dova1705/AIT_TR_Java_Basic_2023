b32d4edff4919f7ee1bb7aa10d369f87737618ed
dova1 <allakov.dovlet@gmail.com>
2023-07-16 20:10:24 +0300

    update

---
+++ Lesson_26/code/hw_26/NumberTest.java
@@ -0,0 +1,10 @@
+package hw_26;
+
+public class NumberTest {
+    public static void main(String[] args) {
+        Integer a = 17;
+        Integer b = 5;
+        Integer c = a + b;
+        System.out.println(c);
+    }
+}
