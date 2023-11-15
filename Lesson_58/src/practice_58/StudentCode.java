39509f500cb148cec8f6543e0422d2fd11574ff4
dova1 <allakov.dovlet@gmail.com>
2023-11-15 23:19:03 +0300

    update

---
+++ Lesson_58/src/practice_58/StudentCode.java
@@ -0,0 +1,73 @@
+package practice_58;
+
+public class StudentCode {
+    public static void main(String[] args) {
+        // создаем два разных экземпляра класса MyThread
+        // вся основная логика заключается в методе run()
+        MyThread myThread1 = new MyThread();
+        MyThread myThread2 = new MyThread();
+
+        // в случае если мы запустим выполнение потоков при помощи метода run(), то выполнение будет находиться
+        // в основном потоке и о параллелизме не может быть и речи, myThread2 не начнет свою работу, пока myThread1 не
+        // выполнит всю логику из метода run();
+//        myThread1.run();
+//        myThread2.run();
+
+
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Runnable + " + i);
+                    try {
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        };
+
+        // для запуска потоков параллельно следует обратиться к методу start(), этот метод создает отдельный поток
+        // выполнения, который не зависит от других потоков и не будет ждать выполнения работы какого-либо потока
+//        myThread1.start();
+//        myThread2.start();
+//        r.run();
+
+        // пример использования конструктора класса Thread, который в качестве аргумента принимает экземпляп
+        // интерфейса Runnable
+        Thread thread1 = new Thread(r);
+
+        // использование анонимного интерфейса в качестве аргумента класса Thread:
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Thread + " + i);
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+
+    static class MyThread extends Thread {
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("MyThread + " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
---
+++ Lesson_58/src/practice_58/StudentCode2.java
@@ -0,0 +1,49 @@
+package practice_58;
+
+public class StudentCode2 {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("1 Thread + " + i);
+                    try {
+                        Thread.sleep(400);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("3 main + " + i);
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
+        // потоки могут начать работу не последоательно, это связано с тем, что инициализация потоков занимает
+        // определенное время и к тому моменту, когда thread2 будет уже работать, thread1 все еще может ждать
+        // освобождения ресурсов для инициализации потока. Данный процесс абсолютно независим
+        thread1.start();
+        thread2.start();
+
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("2 Thread + " + i);
+            try {
+                Thread.sleep(400);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
