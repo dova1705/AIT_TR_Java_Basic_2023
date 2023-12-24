bd432b194549e24695cdb28ae1d8a750c308e873
dova1 <allakov.dovlet@gmail.com>
2023-12-24 22:13:26 +0300

    update

--- consultation/code/bubble_sort/BubbleSort.java
+++ consultation/code/bubble_sort/BubbleSort.java
@@ -3,6 +3,7 @@ package bubble_sort;
 import java.util.Arrays;
 
 public class BubbleSort {
+    // bubble sort O(n^2)
     public static void main(String[] args) {
         int[] numbers = {-1, 0, 7, 6, 5, 4, 3, 2, 1,};
 
